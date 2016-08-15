13649bc33c3f5c3f26ab3a1ba6355bc6c01ac463
brianhouse <brian.house@gmail.com>
2016-08-15 14:01:47 -0400

    fixed js trigger bugs

--- bells.js
+++ bells.js
@@ -1,5 +1,4 @@
 var samples = [[], [], []];
-// var samples = [[]];
 var names = [];
 var playing = false;
 
@@ -8,25 +7,31 @@ function preload() {
     while (bank < samples.length) {
         var i=1;
         while (i < 55) {
-            var num = "" + i;
-            if (num.length < 2) num = "0" + num;
-            var sample = loadSound("samples/belfort" + num + ".MP3")
-            sample.playMode('restart');
-            sample.setVolume(0.25);
-            samples[bank].push(sample);
+            if ([1, 6, 11, 16, 21, 26, 31].indexOf(i) > -1) {   // just grab what we need
+                var num = "" + i;
+                if (num.length < 2) num = "0" + num;
+                console.log("Loading " + "samples/belfort" + num + ".MP3");
+                var sample = loadSound("samples/belfort" + num + ".MP3")
+                sample.playMode('restart');
+                sample.setVolume(0.25);
+                samples[bank].push(sample);
+            } else {
+                samples[bank].push('nop');
+            }
             i++;
         }
         bank++;
     }
 }
 
-function setup() {
+function setup() {    
     var canvas = createCanvas(800, 240);
     canvas.parent("p5");
     for (var d in data) {
         names.push(data[d][0]);
     }
     console.log(names);
+    frameRate(2);
 }
 
 function mouseClicked() {
@@ -34,27 +39,22 @@ function mouseClicked() {
 }
 
 function play() {
-    console.log("Starting...");
-    playing = true;
+    console.log("Playing...");
     for (var d in data) {
         var times = data[d][1];
         var i = names.indexOf(data[d][0]);
-        var s = i*2 + (i * 3);
-        // var pan = ((i / Object.keys(data).length) * 2) - 1;
+        var s = i*2 + (i * 3);                          // finding harmonies
         var pan = (i / Object.keys(data).length);
         if ((i % 2) == 1) pan *= -1;
-        console.log(pan)
         samples[0][s].pan(pan);        
         for (var t in times) {
-            var time = times[t]// * 1000.0;
-            console.log("Sound " + s + " at time " + time);
-            samples[t % samples.length][s].play(time);  // this has a bug and it misses the 0 delay, but sounds nice that way
-            // setTimeout(function(s, t) { return function() { 
-            //     console.log(samples[s]);
-            //     samples[s].play(); 
-            // }}(s), time);
+            var time = times[t];
+            samples[t % samples.length][s].play(time);  // this has a bug and it misses the 0 delay, but it sounds nice that way
         }
     }
+    setTimeout(function() { 
+        window.location.reload(true);
+    }, 70000);
 }
 
 function draw() {
@@ -66,11 +66,8 @@ function draw() {
     var s = second();
 
     if (m == 0 && s == 1 && playing == false) {
-        console.log("PLAY");
         play();
-    }
-    if (m > 0 && playing == true) {
-        playing = false;
+        playing = true;
     }
 
     var m = (60 - m);
@@ -90,10 +87,6 @@ function draw() {
     textAlign(CENTER, CENTER);
     textFont("Monaco");
     textSize(100);
-    clock = text(h + ":" + m + ":" + s, 800/2, 240/2);
-
-    // border
-    // noFill();    
-    // rect(0, 0, 799, 239);         
+    clock = text(h + ":" + m + ":" + s, 800/2, 240/2);   
 
 }
