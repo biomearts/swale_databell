e8505e13ff9776c9efa1498f2c1b18943a6b0a74
brianhouse <brian.house@gmail.com>
2016-08-25 13:48:39 -0400

    p5 not handling background playback well -- switching to howler and timeouts

--- bells.js
+++ bells.js
@@ -1,37 +1,43 @@
-var samples = [[], [], []];
+var samples = [];
 var names = [];
 var playing = false;
 
+/*
+    Using p5 for display and preloading
+    Howler is much better for the audio playback
+    setTimeouts and HackTimer necessary for things to keep time in the background
+*/
+
 function preload() {
-    var bank = 0;
-    while (bank < samples.length) {
-        var i=1;
-        while (i < 55) {
-            if ([1, 6, 11, 16, 21, 26, 31].indexOf(i) > -1) {   // just grab what we need
-                var num = "" + i;
-                if (num.length < 2) num = "0" + num;
-                console.log("Loading " + "samples/belfort" + num + ".MP3");
-                var sample = loadSound("samples/belfort" + num + ".MP3")
-                sample.playMode('restart');
-                sample.setVolume(0.25);
-                samples[bank].push(sample);
-            } else {
-                samples[bank].push('nop');
-            }
-            i++;
+    var i=1;
+    while (i < 55) {
+        if ([1, 6, 11, 16, 21, 26, 31].indexOf(i) > -1) {   // just grab what we need
+            var num = "" + i;
+            if (num.length < 2) num = "0" + num;
+            // console.log("Loading " + "samples/belfort" + num + ".MP3");
+            var sample = new Howl({src: ["samples/belfort" + num + ".MP3"], volume: 0.25});
+            samples.push(sample);
+        } else {
+            samples.push('nop');
         }
-        bank++;
+        i++;
     }
 }
 
-function setup() {    
+function setup() { 
     var canvas = createCanvas(800, 240);
     canvas.parent("p5");
     for (var d in data) {
         names.push(data[d][0]);
     }
-    console.log(names);
     frameRate(2);
+
+    console.log(names);
+    console.log("Loaded at " + hour() + ":" + minute() + ":" + second());        
+    var delay = ((60 - minute()) * 60) + (60 - second())
+    console.log("Remaining seconds: " + delay);
+    setTimeout(play, delay * 1000);
+
 }
 
 function mouseClicked() {
@@ -46,10 +52,13 @@ function play() {
         var s = i*2 + (i * 3);                          // finding harmonies
         var pan = (i / Object.keys(data).length);
         if ((i % 2) == 1) pan *= -1;
-        samples[0][s].pan(pan);        
+        samples[s].stereo(pan);
         for (var t in times) {
-            var time = times[t];
-            samples[t % samples.length][s].play(time);  // this has a bug and it misses the 0 delay, but it sounds nice that way
+            // if (t == 0 || t == times.length -1) continue;   // uncomment to skip the unison hits at the beginning and end
+            var time = times[t] * 1000;
+            setTimeout(function(s, t) { return function() { 
+                samples[s].play(); 
+            }}(s), time);
         }
     }
     setTimeout(function() { 
@@ -64,11 +73,6 @@ function draw() {
     var h = hour() + "";
     var m = minute();
     var s = second();
-
-    if (m == 0 && s == 1 && playing == false) {
-        play();
-        playing = true;
-    }
 
     var m = (60 - m);
     if (m == 60) m = 0;
